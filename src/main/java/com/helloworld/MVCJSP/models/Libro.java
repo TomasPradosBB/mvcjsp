6e19840e9e8745b1f614ca1ddae8b131e4ac4ab2
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-16 10:40:47 +0100

    Clase Libro

---
+++ src/main/java/com/helloworld/MVCJSP/models/Libro.java
@@ -0,0 +1,31 @@
+package com.helloworld.MVCJSP.models;
+
+public class Libro {
+    private String isbdn;
+    private String titulo;
+    private String autor;
+
+    public String getIsbdn() {
+        return isbdn;
+    }
+
+    public void setIsbdn(String isbdn) {
+        this.isbdn = isbdn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
